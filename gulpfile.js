552f6f27ddac50ec42e514a5d3a6659ee5c7d84b
Alex Kafer <kafer005@umn.edu>
2017-09-14 10:38:00 -0500

    Updated gulp to deploy and git ignore to ignore cache

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('build', function (cb) {
+  exec('ng build', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+      .pipe(ghPages({
+        'branch': 'master',
+        'remoteUrl': 'git@github.com:TEDxUMN/tedxumn.github.io.git'
+      }));
+  });
