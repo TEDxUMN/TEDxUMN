85f43b0e78088a13cd16b8079af10e50fa686209
Alex Kafer <kafer005@umn.edu>
2017-11-27 00:40:08 -0600

    Why am I hiding a client side API key?

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,14 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBUAb1ZtS9sJXrt4ozrTIOBDEG12K3nY0A",
+    authDomain: "tedxumn-187016.firebaseapp.com",
+    databaseURL: "https://tedxumn-187016.firebaseio.com",
+    projectId: "tedxumn-187016",
+    storageBucket: "tedxumn-187016.appspot.com",
+    messagingSenderId: "297094860651"
+  }
 };
