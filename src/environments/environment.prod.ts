4b121ff92b1f472e81f86796973fdf6b67c1d452
Alex Kafer <kafer005@umn.edu>
2017-11-28 15:25:15 -0600

    Deploying

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBUAb1ZtS9sJXrt4ozrTIOBDEG12K3nY0A",
+    authDomain: "tedxumn-187016.firebaseapp.com",
+    databaseURL: "https://tedxumn-187016.firebaseio.com",
+    projectId: "tedxumn-187016",
+    storageBucket: "tedxumn-187016.appspot.com",
+    messagingSenderId: "297094860651"
+  }
 };
