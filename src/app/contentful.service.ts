a9485590101c0a474342dd68b21bfe27b50cbe0a
Alex Kafer <kafer005@umn.edu>
2018-03-01 13:39:18 -0600

    Speaker formatting

--- src/app/contentful.service.ts
+++ src/app/contentful.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { createClient, Entry } from 'contentful';
+import { isDevMode } from '@angular/core';
 
 const CONFIG = {
   space: 'y1dvbtp2igep',
   accessToken: 'c9adbea1c0fb218aa26e42932cdb44736d806e55799b1ae1f0c8f350616fb2a1',
-
+  previewToken: '4bf6e874a4457dee888c41c1c32b4817c13feedfe35de9c3f575df788a7fdcde',
   contentTypeIds: {
     speaker: 'speaker',
     event: 'event',
@@ -12,17 +13,28 @@ const CONFIG = {
     teamGroup: 'teamGroup',
     partners: 'partner'
   }
-}
+};
 
 @Injectable()
 export class ContentfulService {
 
-  private cdaClient = createClient({
-    space: CONFIG.space,
-    accessToken: CONFIG.accessToken
-  });
+  private cdaClient;
 
-  constructor() { }
+  constructor() {
+    if (isDevMode()) {
+      this.cdaClient = createClient({
+        space: CONFIG.space,
+        accessToken: CONFIG.previewToken,
+        host: 'preview.contentful.com'
+      });
+      console.log('Using local');
+    } else {
+      this.cdaClient = createClient({
+        space: CONFIG.space,
+        accessToken: CONFIG.accessToken
+      });
+    }
+  }
 
   getEvents(query?: object): Promise<Entry<any>[]> {
     return this.cdaClient.getEntries(Object.assign({
